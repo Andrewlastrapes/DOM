0117f58bd8bba788326d1b85f96b2e27289f1439
Andrew Lastrapes <Andrewlastrapes@gmail.com>
2017-07-18 12:04:00 -0400

    About to use event to make clickable links

---
+++ DOM_practice.js
@@ -0,0 +1,13 @@
+
+
+var photoOne = document.getElementByClassName("thumbnail-image")[0];
+var photoTwo = document.getElementByClassName("thumbnail-image")[1];
+var photoThree = document.getElementByClassName("thumbnail-image")[2];
+var photoFour = document.getElementByClassName("thumbnail-image")[3];
+var photoFive = document.getElementByClassName("thumbnail-image")[4];
+var photoSix = document.getElementByClassName("thumbnail-image")[5];
+
+
+photoOne.addEventListener("click", function(){
+
+})
